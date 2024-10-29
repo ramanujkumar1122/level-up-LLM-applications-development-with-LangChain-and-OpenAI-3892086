75ca4615ff38b0a2f38662a8f59784e997ae148d
Ramanuj Yadav <ramanuj.kumar1122@gmail.com>
2024-10-29 15:11:38 +0530

    changed

--- exercise-files/01/main.py
+++ exercise-files/01/main.py
@@ -1,13 +1,18 @@
 from dotenv import load_dotenv
 from langchain_openai import OpenAI
+from langchain_core.prompts import PromptTemplate
 from colorama import Fore
 
 load_dotenv()
+llm = OpenAI()
+prompt_template = PromptTemplate.from_template("tell me a joke about a {topic}?")
 
 
 def generate(text):
     """ generate text based on the input """
-    pass
+    prompt = prompt_template.format(topic=text)
+    print(prompt)
+    return llm.invoke(text)
 
 
 def start():
