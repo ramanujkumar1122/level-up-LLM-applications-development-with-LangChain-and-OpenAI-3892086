3d6df7b1a89c72d88f418bf015a337d77c92cc1a
Ramanuj Yadav <ramanuj.kumar1122@gmail.com>
2024-10-29 19:03:26 +0530

    changed

--- exercise-files/01/main.py
+++ exercise-files/01/main.py
@@ -1,18 +1,19 @@
 from dotenv import load_dotenv
 from langchain_openai import OpenAI
-from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from colorama import Fore
 
 load_dotenv()
 llm = OpenAI()
 
-prompt_template = PromptTemplate.from_template("Tell me a joke about a {topic}?")
+prompt_template = ChatPromptTemplate.from_template("tell me a short joke about {topic}")
+output_parser = StrOutputParser()
 
 def generate(text):
     """ generate text based on the input """
-    prompt = prompt_template.format(topic=text)
-    print(prompt)
-    return llm.invoke(text)
+    chain = prompt_template | llm | output_parser
+    return chain.invoke({"topic": text})
 
 
 def start():
