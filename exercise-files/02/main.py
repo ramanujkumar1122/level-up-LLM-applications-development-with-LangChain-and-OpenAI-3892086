ada0fb83b6775e70d9a0ee93cff82688ffa0c9ed
Ramanuj Yadav <ramanuj.kumar1122@gmail.com>
2024-10-29 19:54:22 +0530

    changed

--- exercise-files/02/main.py
+++ exercise-files/02/main.py
@@ -1,12 +1,26 @@
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
 
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
 # create a vectorstore & embeddings
+vectorstore = FAISS.from_texts(
+    ["harrison worked at kensho"], embedding=OpenAIEmbeddings()
+)
 
 # querying the vectorstore
+query = "Where did Harrison work?"
+docs = vectorstore.similarity_search(query, k=1)
+print(docs[0].page_content)
 
 # querying as retriever
 
